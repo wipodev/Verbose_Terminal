f8e60f9d409ad0578a57a1f79e283e698601cad3
wipodev <ajwipo@gmail.com>
2024-05-28 10:36:13 -0500

    Refactored verbose_terminal package to use styles from separate styles.py file

--- verbose_terminal/main.py
+++ verbose_terminal/main.py
@@ -1,16 +1,4 @@
-# TERMINAL COLORS (ANSI code)
-styles = {
-    'success': '\033[1;32;42m',     # light green text, green background
-    'warning': '\033[1;33;43m',     # light yellow text, yellow background
-    'error': '\033[1;31;41m',       # light red text, red background
-    'info': '\033[1;36;44m',        # light cyan text, blue background
-    'success_light': '\033[1;32m',  # light green text
-    'warning_light': '\033[1;33m',  # light yellow text
-    'error_light': '\033[1;31m',    # light red text
-    'info_light': '\033[1;36m'      # light cyan text
-}
-
-reset = '\033[0m' # resetear al estilo por defecto
+from .styles import styles, reset
 
 class Msg:
     @staticmethod
@@ -55,15 +43,11 @@ class Msg:
 
 if __name__ == '__main__':
   # Examples of use
-  msg("This is a success message with background", True, 'success')
-  msg("This is a warning message with background", True, 'warning')
-  msg("This is an error message with background", True, 'error')
-  msg("This is an info message with background", True, 'info')
-  msg("This is a success message light", True, 'success_light')
-  msg("This is a warning message light", True, 'warning_light')
-  msg("This is an error message light", True, 'error_light')
-  msg("This is an info message light", True, 'info_light')
-  msg("This is a plain message", True, 'reset')
-  msg("This is a plain message", True)
-  msg("This is a simple message, which will not seem", False)
-  msg("This is a simple message, which will not seem")
+  Msg.success("Test message", True)
+  Msg.warning("Test message", True)
+  Msg.error("Test message", True)
+  Msg.info("Test message", True)
+  Msg.success_light("Test message", True)
+  Msg.warning_light("Test message", True)
+  Msg.error_light("Test message", True)
+  Msg.info_light("Test message", True)
---
+++ verbose_terminal/styles.py
@@ -0,0 +1,13 @@
+# TERMINAL COLORS (ANSI code)
+styles = {
+    'success': '\033[1;32;42m',     # light green text, green background
+    'warning': '\033[1;33;43m',     # light yellow text, yellow background
+    'error': '\033[1;31;41m',       # light red text, red background
+    'info': '\033[1;36;44m',        # light cyan text, blue background
+    'success_light': '\033[1;32m',  # light green text
+    'warning_light': '\033[1;33m',  # light yellow text
+    'error_light': '\033[1;31m',    # light red text
+    'info_light': '\033[1;36m'      # light cyan text
+}
+
+reset = '\033[0m' # reset to default style
