49087544aa47e90b77ce8fd2c061e041af8b92f3
wipodev <ajwipo@gmail.com>
2024-05-28 22:33:54 -0500

    removed unnecessary file, modified comments in styles and minor changes in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='verbose-terminal',
-    version='0.0.1',
+    version='1.0.0',
     author='Wipodev',
     author_email='ajwipo@gmail.com',
-    description='Verbose Terminal',
+    description='A Python library for displaying messages in the terminal with styles and logging support.',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/wipodev/Verbose_Terminal',
@@ -24,6 +23,4 @@ setup(
     license='MIT',
     keywords='python, terminal, console, verbose, verbose terminal, verbose terminal, verbose terminal',
     packages=find_packages(exclude=('tests*', 'testing*', 'test*')),
-    install_requires=[
-    ],
 )
--- verbose_terminal/styles.py
+++ verbose_terminal/styles.py
@@ -1,12 +1,12 @@
 # TERMINAL COLORS (ANSI code)
 styles = {
     'log': '\033[1;37m',             # white text
-    'debug': '\033[1;34m',           # light blue text
-    'info': '\033[1;36m',            # light cyan text
-    'success': '\033[1;32m',         # light green text
-    'warning': '\033[1;33m',         # light yellow text
+    'debug': '\033[1;34m',           # blue text
+    'info': '\033[1;36m',            # cyan text
+    'success': '\033[1;32m',         # green text
+    'warning': '\033[1;33m',         # yellow text
     'error': '\033[1;37;41m ',       # white text, red background
-    'critical': '\033[1;31;40m ',    # light red text, black background
+    'critical': '\033[1;31;40m ',    # red text, black background
 }
 
 reset = ' \033[0m' # reset to default style
