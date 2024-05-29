22725a99acba80ace207393501133ed7bb32a63a
wipodev <ajwipo@gmail.com>
2024-05-28 22:02:30 -0500

    modified test to adapt to changes in the main class and added test for log methods

--- tests/test_verbose_terminal.py
+++ tests/test_verbose_terminal.py
@@ -1,61 +1,130 @@
-from verbose_terminal import Msg
+import os
+import pytest
+from verbose_terminal import console
 
-def test_success_verbose(capsys):
-    Msg.success("Test success message", verbose=True)
+def test_log_verbose(capsys):
+    console.log("Test log message")
     captured = capsys.readouterr()
     print(captured.out)
-    assert "success message: Test success message" in captured.out
+    assert "Test log message" in captured.out
 
-def test_warning_verbose(capsys):
-    Msg.warning("Test warning message", verbose=True)
+def test_debug_verbose(capsys):
+    console.debug("Test debug message")
     captured = capsys.readouterr()
     print(captured.out)
-    assert "warning message: Test warning message" in captured.out
-
-def test_error_verbose(capsys):
-    Msg.error("Test error message", verbose=True)
-    captured = capsys.readouterr()
-    print(captured.out)
-    assert "error message: Test error message" in captured.out
+    assert "Test debug message" in captured.out
 
 def test_info_verbose(capsys):
-    Msg.info("Test info message", verbose=True)
+    console.info("Test info message")
     captured = capsys.readouterr()
     print(captured.out)
-    assert "info message: Test info message" in captured.out
+    assert "Test info message" in captured.out
 
-def test_success_light_verbose(capsys):
-    Msg.success_light("Test success light message", verbose=True)
+def test_success_verbose(capsys):
+    console.success("Test success message")
     captured = capsys.readouterr()
     print(captured.out)
-    assert "success message: Test success light message" in captured.out
+    assert "Test success message" in captured.out
 
-def test_warning_light_verbose(capsys):
-    Msg.warning_light("Test warning light message", verbose=True)
+def test_warning_verbose(capsys):
+    console.warning("Test warning message")
     captured = capsys.readouterr()
     print(captured.out)
-    assert "warning message: Test warning light message" in captured.out
+    assert "Test warning message" in captured.out
 
-def test_error_light_verbose(capsys):
-    Msg.error_light("Test error light message", verbose=True)
+def test_error_verbose(capsys):
+    console.error("Test error message")
     captured = capsys.readouterr()
     print(captured.out)
-    assert "error message: Test error light message" in captured.out
+    assert "Test error message" in captured.out
 
-def test_info_light_verbose(capsys):
-    Msg.info_light("Test info light message", verbose=True)
+def test_critical_verbose(capsys):
+    console.critical("Test critical message")
     captured = capsys.readouterr()
     print(captured.out)
-    assert "info message: Test info light message" in captured.out
-
-def test_simple_message(capsys):
-    Msg.msg("Test simple message", verbose=True)
-    captured = capsys.readouterr()
-    print(captured.out)
-    assert "Test simple message" in captured.out
+    assert "Test critical message" in captured.out
 
 def test_verbose_false(capsys):
-    Msg.success("Test verbose false message", verbose=False)
+    console.success("Test verbose false message", verbose=False)
     captured = capsys.readouterr()
     print(captured.out)
     assert "" in captured.out
+
+def test_all_logs_level():
+    log_file = 'test_logs.log'
+    console.enable_logging(log_file=log_file, log_level='DEBUG')
+    
+    console.log("This is a test log message")
+    console.success("This is a test success message")
+    console.info("This is a test info message")
+    console.debug("This is a test debug message")
+    console.warning("This is a test warning message")
+    console.error("This is a test error message")
+    console.critical("This is a test critical message")
+    
+    logs = console.view_logs()
+    
+    assert "This is a test log message" in logs
+    assert "This is a test success message" in logs
+    assert "This is a test info message" in logs
+    assert "This is a test debug message" in logs
+    assert "This is a test warning message" in logs
+    assert "This is a test error message" in logs
+    assert "This is a test critical message" in logs
+    console.clear_logs()
+
+def test_view_logs_no_logging_enabled(capsys):
+    console.disable_logging()
+    console.view_logs()
+    captured = capsys.readouterr()
+    print(captured.out)
+    assert "Logging is not enabled. No logs to clear." in captured.out
+
+def test_view_logs(capsys):
+    console.enable_logging('test_logs.log', 'DEBUG')
+    console.debug("Debug message", verbose=True)
+    console.info("Info message", verbose=True)
+    logs = console.view_logs()
+    assert "Debug message" in logs
+    assert "Info message" in logs
+    console.clear_logs()
+
+def test_clear_logs_no_logging_enabled(capsys):
+    console.disable_logging()
+    console.clear_logs()
+    captured = capsys.readouterr()
+    print(captured.out)
+    assert "Logging is not enabled. No logs to clear." in captured.out
+
+def test_clear_logs(capsys):
+    console.enable_logging('test_logs.log', 'DEBUG')
+    console.debug("Debug message", verbose=True)
+    console.info("Info message", verbose=True)
+    console.clear_logs()
+    logs = console.view_logs()
+    assert logs == ""
+    console.clear_logs()
+
+def test_save_logs_no_logging_enabled(capsys):
+    console.disable_logging()
+    console.save_logs('saved_logs.log')
+    captured = capsys.readouterr()
+    print(captured.out)
+    assert "Logging is not enabled. No logs to clear." in captured.out
+
+def test_save_logs(capsys):
+    console.enable_logging('test_logs.log', 'DEBUG')
+    console.debug("Debug message", verbose=True)
+    console.info("Info message", verbose=True)
+    console.save_logs('saved_logs.log')
+    assert os.path.exists('saved_logs.log')
+    console.clear_logs()
+
+# Cleanup
+def teardown_module(module):
+    try:
+        console.disable_logging()  # Disable logging to release the log file
+        os.remove('test_logs.log')
+        os.remove('saved_logs.log')
+    except FileNotFoundError:
+        pass
